e5ac14775f941e6e513d180bc717ff16737f24f7
Aa-Ritchey <m4jxdryzub@snkmail.com>
2017-02-19 21:33:43 -0800

    add a WordPress plugin

---
+++ plugin-e38b19/plugin-e38b19.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+Plugin Name: Empty Plugin
+Description: Does absolutely nothing.
+Version: 1.0
+Author: Aaron Ritchey
+Author URI: http://aaronritchey.com
+*/
+
+// Prevent direct access to this file.
+defined( 'ABSPATH' ) or die();
+
+// Does absolutely nothing.
